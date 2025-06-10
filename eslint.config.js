25ecb19eeddc27f0023710625fe6aea7b9a218bb
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-10 19:30:32 +0700

    build: simplify build and lint configuration
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,27 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactDom from "eslint-plugin-react-dom";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import reactX from "eslint-plugin-react-x";
+import { globalIgnores } from "eslint/config";
+import reactPlugin from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  globalIgnores(["dist"]),
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat["jsx-runtime"],
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-        tsconfigRootDir: import.meta.dirname,
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-    plugins: {
-      "react-dom": reactDom,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-x": reactX,
-    },
-    rules: {
-      ...reactDom.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      ...reactX.configs["recommended-typescript"].rules,
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.js"],
+        },
+      },
     },
   },
 );
