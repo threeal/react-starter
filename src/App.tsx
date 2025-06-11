7013c4a5a053ea3b1d008aa001bd1c5f2e09e3b8
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-11 18:11:55 +0700

    feat: remove Vite logo in the sample app
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import reactLogo from "/react.svg";
 import "./App.css";
 
 function App() {
@@ -9,14 +8,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>React</h1>
       <div className="card">
         <button
           type="button"
@@ -30,9 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the React logo to learn more</p>
     </>
   );
 }
