a24340db28982323ea2435f89f86e8c49d042831
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-12 11:53:48 +0700

    feat: simplify React source code (#436)
    * feat: remove Vite logo in the sample app
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: remove unused elements in `App.tsx`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: create root only if element found without throwing error
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: rename `main.tsx` to `index.tsx`
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 const root = document.getElementById("root");
-if (!root) {
-  throw new Error("Root element not found");
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
 }
-
-createRoot(root).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
