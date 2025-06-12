9ea2599926832c550b8caaf7809ff6661856e9bd
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-12 18:33:42 +0700

    build: use dynamic base in `vite.config.ts` based on `GITHUB_REPOSITORY` env
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+let base: string | undefined;
+if (process.env.GITHUB_REPOSITORY) {
+  base = process.env.GITHUB_REPOSITORY.slice(
+    process.env.GITHUB_REPOSITORY.indexOf("/"),
+  );
+}
+
 export default defineConfig({
-  base: "/react-starter",
+  base,
   plugins: [react()],
 });
